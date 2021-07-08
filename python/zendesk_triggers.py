a16e377bf674c623f54b39a91fb3db05b16826e8
famazak <zakfama58@gmail.com>
2021-07-08 14:21:22 -0400

    add script to parse zendesk triggers to csv

---
+++ python/zendesk_triggers.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+with open("/Volumes/GoogleDrive/Shared drives/analytics/data/zendesk_triggers.json", "r") as json_file:
+    data = json.load(json_file)
+    triggers = data["triggers"]
+
+outfile = open("/Volumes/GoogleDrive/Shared drives/analytics/data/out/zendesk_triggers.csv", "w")
+csv_file = csv.writer(outfile)
+csv_file.writerow(['id', 'title', 'active', 'condition_field', 'condition_value'])
+for t in triggers:
+    for f in t["conditions"]["any"]:
+        id = t["id"]
+        title = t["title"]
+        active = t["active"]
+        field = f["field"]
+        brand_id = f["value"]
+        csv_file.writerow([id, title, active, field, brand_id])
